5eac3879f0c0f401fcb6cbb958814b4d9f0ca4c0
Anas-Rabeea <AnasRabeea001@gmail.com>
2026-01-14 17:28:59 +0200

    Adding sendVerificationEmail Logic

--- src/main/java/com/anascoding/auth_system/service/EmailVerificationService.java
+++ src/main/java/com/anascoding/auth_system/service/EmailVerificationService.java
@@ -19,5 +19,28 @@ public class EmailVerificationService {
     public void sendVerificationEmail(@Email @NotBlank String email) {
 
 
+        String token =  verificationTokenService.generateEmailVerificationToken(10);
+        // Token_2RI#@L : To not make token guessing easy (prevents brute-force)
+        String redisKey = "Token_2RI#@L:" + token;
+        // save  the token with email in redis
+        redisTemplate
+                .opsForValue()
+                .set(redisKey,email ,token_ttl);
+
+        String verificationLink = "http://localhost:8055/api/v1/auth/verify-email?token=" + token;
+        // send the email
+        String verificationEmailContent = """
+                    Hello %s,
+                    Please verify your account by clicking the link below:
+                    %s
+                    NOTE: This link expires in 3 minutes.
+                    If you did not make this request, please disregard this email.
+                """.formatted(email,verificationLink) ;
+        String verificationEmailTitle = "Your Verification Code For Mehna";
+        this.emailSenderService.send(
+                email,
+                verificationEmailContent,
+                verificationEmailTitle
+        );
     }
 }
