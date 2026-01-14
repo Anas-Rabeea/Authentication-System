165f1d7b5f2a35e1b750910e7085070fca78523c
Anas-Rabeea <AnasRabeea001@gmail.com>
2026-01-14 03:21:26 +0200

    Adding EmailSenderService Interface

--- src/main/java/com/anascoding/auth_system/service/EmailVerificationService.java
+++ src/main/java/com/anascoding/auth_system/service/EmailVerificationService.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 public class EmailVerificationService {
 
     // Key = Email , Value = Token
+    private static final Duration token_ttl = Duration.ofMinutes(10);
+    private final RedisTemplate<String,String> redisTemplate;
+    private final EmailSenderService emailSenderService;
 
     @Async
     public void sendVerificationEmail(@Email @NotBlank String email) {
