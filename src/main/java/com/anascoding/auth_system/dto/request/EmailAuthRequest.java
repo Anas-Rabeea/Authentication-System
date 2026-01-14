921ac68310b04b25f74d6db182232baa77354995
Anas-Rabeea <AnasRabeea001@gmail.com>
2026-01-15 01:45:32 +0200

    Solving Bug in Validation annotations in EmailAuthRequest

--- src/main/java/com/anascoding/auth_system/dto/request/EmailAuthRequest.java
+++ src/main/java/com/anascoding/auth_system/dto/request/EmailAuthRequest.java
@@ -5,9 +5,8 @@ import com.anascoding.auth_system.validator.StrongPassword.StrongPassword;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
@@ -15,13 +14,13 @@ public record EmailAuthRequest(
 
         @Email(message = "Enter a valid email address.")
         @NotBlank(message = "Email cant be empty.")
+        @Size(max = 100 , message = "email length must be between 11 and 99")
         @NonDisposalEmail
         String email,
 
         @NotBlank(message = "Password cant be empty.")
-//        @Min(value = 10, message = "Password Should be at least 10")
-//        @Max(value = 100, message = "Password Should be at most 100")
-//        @StrongPassword
+        @Size(min = 10,max = 100 , message = "Password length must be between 11 and 99")
+        @StrongPassword
         String password,
 
         @Enumerated(EnumType.STRING)
