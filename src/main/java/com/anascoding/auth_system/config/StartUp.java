51f1b66af344b6c71c85b84143608b56235316ff
Anas-Rabeea <AnasRabeea001@gmail.com>
2026-01-14 20:32:11 +0200

    Adding a StartUp To Initialize test Data in the Database

---
+++ src/main/java/com/anascoding/auth_system/config/StartUp.java
@@ -0,0 +1,48 @@
+package com.anascoding.auth_system.config;
+
+import com.anascoding.auth_system.entity.AppAuthProvider;
+import com.anascoding.auth_system.entity.AppUser;
+import com.anascoding.auth_system.entity.Role;
+import com.anascoding.auth_system.repository.AppUserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StartUp implements CommandLineRunner {
+
+    private final AppUserRepo appUserRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if(appUserRepo.count() == 0){
+
+            AppUser appUserDeveloper =
+                    AppUser
+                        .builder()
+                        .email("developer-backend@mehna.com")
+                        .password(passwordEncoder.encode("P#@!sswor$#!!99"))
+                        .role(Role.DEVELOPER)
+                        .appAuthProvider(AppAuthProvider.LOCAL)
+                        .emailVerified(true)
+                        .build();
+            AppUser appUserAdmin =
+                    AppUser
+                            .builder()
+                            .email("admin-backend@mehna.com")
+                            .password(passwordEncoder.encode("P#@!sswor$#!!99"))
+                            .role(Role.ADMIN)
+                            .appAuthProvider(AppAuthProvider.LOCAL)
+                            .emailVerified(true)
+                            .build();
+
+        }
+
+
+
+    }
+}
